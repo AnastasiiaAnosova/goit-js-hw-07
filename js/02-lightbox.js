a97cfdff8a86afb8f69d439da9c7e2632b88e11d
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-07-24 16:55:38 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector('.gallery');
+const galleryItemsElement = galleryItems.map((image) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+    <img 
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+    />
+    </a>
+    </li>`;
+}).join('');
+
+galleryElement.innerHTML = galleryItemsElement;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
