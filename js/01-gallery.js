c4dad3f40ef1bd9e998106b309424b440dbc943b
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-07-24 16:34:48 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryElement = document.querySelector('.gallery');
+const galleryItemsElement = galleryItems.map((image) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+    <img 
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+    />
+    </a>
+    </li>`;
+}).join('');
 
-console.log(galleryItems);
+galleryElement.innerHTML = galleryItemsElement;
+//first way
+// let instance;
+    
+// galleryElement.addEventListener('click', openLightBox);
+
+// function openLightBox(event) {
+//     //cancellation of default events
+//     event.preventDefault();
+//     if (!event.target.nodeName === 'IMG') {
+//         return;
+//     }
+//     instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}"/>`);
+//     instance.show();
+// }
+
+// document.addEventListener('keydown', closeGallery);
+// function closeGallery(event) {
+//     if(event.code === 'Escape') instance.close();
+// }
+
+//second way
+galleryElement.addEventListener('click', showImg);
+
+const instance = basicLightbox.create(`<img/>`, {
+    onShow: (instance) => {
+        document.addEventListener('keydown', closeImg);
+    },
+    onClose: (instance) => {
+        document.removeEventListener('keydown', closeImg);
+    }
+});
+
+function showImg(event) {
+    event.preventDefault();
+    // console.dir(instance.element());
+    instance.element().querySelector('img').src = event.target.dataset.source;
+    instance.show();
+}
+
+function closeImg(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
